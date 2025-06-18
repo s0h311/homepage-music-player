46d737f504711c87f6c16aabaa3ba8ca52d71ed3
Soheil Nazari <kontakt@soheilnazari.de>
2025-06-18 20:30:36 +0200

    install three.js and tresjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { templateCompilerOptions } from '@tresjs/core'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      ...templateCompilerOptions,
+    }),
+  ],
   server: {
     port: 3000,
   },
